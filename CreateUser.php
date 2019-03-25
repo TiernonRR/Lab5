69385e52e4dc6aa5f29598dc19777329e847801c
TiernonRR <37198577+TiernonRR@users.noreply.github.com>
2019-03-24 19:01:29 -0500

    Add files via upload

---
+++ CreateUser.php
@@ -0,0 +1,20 @@
+<?php
+  $userName = $_POST['userID'];
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "t445r565", "Eiph7Foh", "t445r565");
+  /* connection check */
+  if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+  }
+
+
+    $addUser = "INSERT INTO Users(user_id) VALUES ('" . $userName . "')";
+    echo $addUser;
+    if($newLogin = $mysqli->query($addUser)){
+      echo "Login Successful";
+    }
+    else{
+      echo "Unable to login";
+    }
+
+ ?>
